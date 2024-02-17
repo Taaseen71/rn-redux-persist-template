40122ef9f4e4f9a156c7f5a7b9b3fffa44bfb381
Saadat Taaseen <44211184+Taaseen71@users.noreply.github.com>
2024-02-17 18:29:15 -0500

    Update index.jsx

--- template/src/screens/SignUp/index.jsx
+++ template/src/screens/SignUp/index.jsx
@@ -1,14 +1,38 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState, useEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
-
+import globalStyle from 'src/styles/GlobalStyles'
+import { useDispatch, useSelector } from 'react-redux';
 
 const SignUp = () => {
   const navigation = useNavigation()
+  const [pwd, setPwd] = useState('')
+  const [email, setEmail] = useState('')
+  const dispatch = useDispatch();
   
   return (
     <View>
       <Text>SignUp</Text>
+      <TextInput 
+        style={globalStyle().TextInputComponent}
+        onChangeText={setEmail} 
+        value={email} 
+        placeholder={"Enter Email"}
+      />
+      <TextInput 
+        style={globalStyle().TextInputComponent}
+        value={pwd} 
+        onChangeText={setPwd} 
+        placeholder={"Enter Pwd"}
+      />
+      <TouchableOpacity 
+        style={globalStyle().button} 
+        onPress={() => {
+          dispatch(logInUser(email))
+        }}
+      >
+        <Text>Submit</Text>
+      </TouchableOpacity>
       <Button title="Have an account? Log In" onPress={() => {navigation.navigate('Log In')}}/>
       <Button title="useContext" onPress={() => {navigation.navigate('ReactContext')}}/>
     </View>
