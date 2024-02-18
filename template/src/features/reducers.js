8c14af8f1591833f34d04e3f1e001a73cc9e79d6
Saadat Taaseen <44211184+Taaseen71@users.noreply.github.com>
2024-02-17 19:17:23 -0500

    Update reducers.js

--- template/src/features/reducers.js
+++ template/src/features/reducers.js
@@ -1,4 +1,5 @@
 import userSlice from "./user/userSlice";
 import counterSlice from "./counter/counterSlice";
+import itemSlice from "./item/itemSlice";
 
-export default {counter: counterSlice, user: userSlice};
+export default {counter: counterSlice, user: userSlice, item: itemSlice};
