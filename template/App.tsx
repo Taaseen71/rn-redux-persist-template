5c3e6c4261b59a12736ff6c04584afd3ab066a96
Saadat Taaseen <login@saadt.dev>
2024-03-26 10:26:01 -0400

    working without nav

--- template/App.tsx
+++ template/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import Navigation from 'src/navigation';
 import ErrorBoundary from 'src/components/ErrorBoundary';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
 // import store from 'src/store';
 import {store} from 'src/store';
+import NavigationDrawer from 'src/navigation/NavigationDrawer';
+// import 'react-native-gesture-handler';
 
 function App(): JSX.Element {
- 
   return (
     <Provider store={store}>
       <ErrorBoundary fallback={'There was an error'}>
         <NavigationContainer>
-              <Navigation />
+          <Navigation />
+          {/* <NavigationDrawer /> */}
         </NavigationContainer>
       </ErrorBoundary>
     </Provider>
   );
 }
-
 
 export default App;
